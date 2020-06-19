f4c1ea98aa47f5ba4e5dc0e9272070091ca07730
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-06-19 16:28:41 +0100

    Deployment - New Frontend build folder

---
+++ frontend/build/precache-manifest.84fe86ab376da39513e1c86748ff0591.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816d0e8fdd00d6985217775ef8ec3653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "119b513b69831f9b58a7",
+    "url": "/static/css/2.b81c626b.chunk.css"
+  },
+  {
+    "revision": "375c631c77a38bab20f4",
+    "url": "/static/css/main.c401d07e.chunk.css"
+  },
+  {
+    "revision": "119b513b69831f9b58a7",
+    "url": "/static/js/2.e7039631.chunk.js"
+  },
+  {
+    "revision": "58b05ca66b7fee124de228c812ae6027",
+    "url": "/static/js/2.e7039631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375c631c77a38bab20f4",
+    "url": "/static/js/main.3324d6e7.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
