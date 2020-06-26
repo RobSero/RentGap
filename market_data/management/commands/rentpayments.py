172c2a3be6204de7e1aebffb8fc348ce01680177
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-06-26 17:38:00 +0100

    BackEnd - Added two new custom django management commands - updatemarketdata and payrent. Heroku upload and schedulers set

---
+++ market_data/management/commands/rentpayments.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from orders.models import Order
+from properties.models import Property
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+    
+# THIS HANDLES THE PAYMENTS INTO USERS ACCOUTS BASED ON HOW MUCH MONTHLY RENTAL INCOME THEY OWN. FUNCTION WILL RUN DAILY
+class Command(BaseCommand):
+  help = 'Will make payments into all users accounts based on how much rental income they are owed'
+  
+  def handle(self, *args, **options):
+  # Get All Orders
+    all_orders = Order.objects.filter(active=True)
+    # loop through orders
+    for order in all_orders:
+      # get invested property
+      invested_property = Property.objects.get(pk=order.property_detail.id)
+      # get user
+      user = User.objects.get(pk=order.user.id)
+      # calculate rental
+      rental_amount_owed = invested_property.rental_value * order.ownership
+      # add to account - This is divided by 30.42 as this function will run daily instead of monthly. limitations of heroku free version of cron
+      user.money += (rental_amount_owed / 30.42)
+      user.save()
---
+++ market_data/management/commands/updatemarketdata.py
@@ -0,0 +1,43 @@
+from django.core.management.base import BaseCommand
+from market_data.property_param_data_many import all_prop_growth_params, all_property_params
+from properties.models import Property
+from rest_framework.exceptions import NotFound
+import random
+import requests
+from time import sleep
+
+def get_property(pk): 
+    try:
+      return Property.objects.get(pk=pk)
+    except Property.DoesNotExist:
+      raise NotFound()
+    
+# returns a value roughly between 98%-102% of argument
+def random_value_changer(value):
+  return random.randint(int(value * 0.98), int(value * 1.02))
+
+
+
+class Command(BaseCommand):
+  help = 'Will artificial adjust some property values at random every few hours'
+  
+  def handle(self, *args, **options):
+    url_path_value = 'https://api.propertydata.co.uk/valuation-sale'
+    url_path_rent = 'https://api.propertydata.co.uk/valuation-rent'
+    # loop through each property and send a request to property data to collect latest values and rent
+    for property_params in all_property_params:
+      # Make HTTP request to third party API - Get latest property value
+      http_response_value = requests.get(url_path_value, params=property_params)
+      # PropertyData requires 3-5 second delay between requests or they will decline
+      sleep(3)
+      # Make HTTP request to third party API - Get latest property rental
+      http_response_rent = requests.get(url_path_rent, params=property_params)
+      property_value = http_response_value.json()
+      property_rent = http_response_rent.json()
+    #Get property and update with new values
+      property_to_update = get_property(pk=property_params['database_ref'])
+      property_to_update.current_valuation = property_value['result']['estimate']
+      property_to_update.margin = property_value['result']['margin']
+      property_to_update.rental_value = property_rent['result']['estimate']
+      property_to_update.save()
+      sleep(5)
